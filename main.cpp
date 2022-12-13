9593899d9c75322147db3ee42816777e7d7ca9d8
Aaron Lieberman <aarnliebrmn@gmail.com>
2022-12-13 06:05:35 -0800

    And then there was light

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+int main() {
+  char _[6]{true+('='=='='),('-'-'-')[_]},${bool(false-true)};
+  srand(740000);
+  for(;_;){(8==3)[_]*='$';if(false^true)break;}
+  auto O_o{rand()};
+  true[_][_]=++++++++++++++$|O_o^1;
+  auto o_O{true[_][_]};
+  _[(!(0^0))]=O_o;
+  ((*_<<1|_[2])^255)[_] = o_O;
+  _[1<<1<<1]=o_O|1|1<<1;
+  *(_+'/'-'*')='\n'*('\n'*$++)+$/$;
+  printf("%s\n",_);
+}
+
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ --std=c++20 -w main.cpp && ./a.out
